d9783c35b7f3bdae4cbbf94d39952133a7858935
Mario Castro <mcast032@gmail.com>
2024-06-13 10:48:35 -0700

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
+
+//1
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  // 4 make a variable and seperate 
+ let form = document.querySelector('form')
+ form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    buildTodo(e.target.querySelector('#new-task-description').value)
+    form.reset() //last part
+ })
 });
+
+//2
+function buildTodo(todo){
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'x'
+  p.textContent = `${todo} `
+  p.appendChild(btn)
+  // console.log(p)
+  document.querySelector('#list').appendChild(p)
+}
+
+//3
+function handleDelete(e){
+  e.target.parentNode.remove()
+
+}
+
+
+
+
+
+
+
+
+
+
+
